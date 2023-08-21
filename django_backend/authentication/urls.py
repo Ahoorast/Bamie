5310ffc40616995bb3022c71cf653326e1865140
Ahoorast <AhooraBozorgManesh@Gmail.com>
2023-08-21 21:28:09 +0330

    knox backend token auth setup

---
+++ django_backend/authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from knox import views as knox_views
+from . import views
+
+urlpatterns = [
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('logout/', knox_views.LogoutView.as_view(), name="knox_logout"),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name="knox_logoutall"),
+    path('signup/', views.SignupView.as_view(), name="signup"),
+]
--- django_backend/authentication/views.py
+++ django_backend/authentication/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.models import User, Group
 
-# Create your views here.
+
+from rest_framework import permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+from knox.views import LoginView as KnoxLoginView
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+
+
+class LoginView(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginView, self).post(request, format=None)
+    
+class SignupView(APIView):
+    @staticmethod
+    def post(request):
+        try:
+            username = request.data.get('username')
+            password = request.data.get('password')
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        try: 
+            validate_password(password)
+        except ValidationError as e:
+            return Response({'messages': e.messages}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            User.objects.create_user(username=username, password=password)
+        except:
+            return Response({'messages': 'username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_200_OK)
