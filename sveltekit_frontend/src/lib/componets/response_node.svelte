02842c768e62cf95627f839e5f8049239c73efc3
barbodak <barbodak@gmail.com>
2023-08-21 22:55:15 +0330

    dragable added

---
+++ sveltekit_frontend/src/lib/componets/response_node.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { draggable } from '@neodrag/svelte';
+    export let position = {
+    x : 0,
+    y : 0,
+};
+</script>
+
+<div  use:draggable={{ position: position, bounds: 'parent' }}
+class="w-5 h-5 bg-gray-100 border border-gray-500 rounded-md">
+    yo turns out we had a secret wepon
+</div>
