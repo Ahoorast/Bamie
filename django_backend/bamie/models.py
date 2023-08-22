4f02adce5db235d99091a814d5ebe21cd7e11d4e
Ahoorast <AhooraBozorgManesh@Gmail.com>
2023-08-22 19:34:40 +0330

    guidance tree basic model

--- django_backend/bamie/models.py
+++ django_backend/bamie/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class GuidanceTree(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    # TODO: maybe better way to keep a tree?
+    parents_array = ArrayField(
+        models.IntegerField(default=-1)
+    )
+    # TODO: store how the front positions the flowchart for keeping & showing
