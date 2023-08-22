5bca82da15f834e0454348839aba95edbc8a1ca9
Ahoorast <AhooraBozorgManesh@Gmail.com>
2023-08-22 19:26:11 +0330

    added svelvet

---
+++ sveltekit_frontend/src/lib/components/Canvas.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { Node, Svelvet } from 'svelvet';
+</script>
+
+<Svelvet class="w-full h-full" height="{1000}"TD minimap locked>
+    <Node let:grabHandle locked={true} id="alpha" bgColor="red" label="Default Node">
+        <!-- <div class="">
+            <div class="w-full">
+                <textarea class="w-full" type="text" rows="5"                 placeholder="sample expected request leading to this e.g. "/>
+            </div>
+            <div class="w-full">
+                <textarea class="w-full" type="text" rows="5" 
+                placeholder="sample expected response for this e.g. "/>
+            </div>
+        </div> -->
+    </Node>
+</Svelvet>
+
--- sveltekit_frontend/src/routes/canvas/+page.svelte
+++ sveltekit_frontend/src/routes/canvas/+page.svelte
@@ -1,15 +1,5 @@
 <script>
-    export let data;
-    import ResponseNode from "$lib/components/ResponseNode.svelte";
-    let position_1 = {
-        x : 0,
-        y : 0,
-    };
-    let position_2 = {
-        x : 5,
-        y : 5,
-    };
+    import Canvas from "$lib/components/Canvas.svelte";
 </script>
 
-<ResponseNode position = {position_1}/>
-<ResponseNode position = {position_2}/>
+<Canvas/>
