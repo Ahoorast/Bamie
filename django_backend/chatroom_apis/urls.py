20ef04261833ca8bd3e485e7333213af7830a1d2
Ahoorast <AhooraBozorgManesh@Gmail.com>
2023-08-22 23:04:04 +0330

    chat room api urls

--- django_backend/chatroom_apis/urls.py
+++ django_backend/chatroom_apis/urls.py
@@ -3,10 +3,6 @@ from knox import views as knox_views
 from . import views
 
 urlpatterns = [
-    # path('login/', views.LoginView.as_view(), name="login"),
-    # path('logout/', knox_views.LogoutView.as_view(), name="knox_logout"),
-    # path('logoutall/', knox_views.LogoutAllView.as_view(), name="knox_logoutall"),
-    # path('signup/', views.SignupView.as_view(), name="signup"),
     path('create/', views.ChatRoomAPIView.as_view({
         'post': 'create',
     }), name="create_chat_room"),
@@ -20,4 +16,3 @@ urlpatterns = [
         'put': 'push_message',
     }), name="push_message_to_chat_room"),
 ]
-    
