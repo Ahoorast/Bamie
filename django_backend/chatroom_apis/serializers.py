cad913349c21b5cc995b01abd8c8469c1d7adf16
Ahoorast <AhooraBozorgManesh@Gmail.com>
2023-08-23 17:19:58 +0330

    push_message chatroom api front and back + serializer fix + extra package.json node_modules removed

--- django_backend/chatroom_apis/serializers.py
+++ django_backend/chatroom_apis/serializers.py
@@ -8,11 +8,13 @@ class ChatRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatRoom
         fields = '__all__'
+    
     def validate_guidance_tree(self, guidance_tree):
         owner = self.initial_data.get('owner')
         if guidance_tree.owner.pk != owner:
             raise ValidationError("the owner of the tree and the chatroom should be the same of the guidance tree")
-        
+        return guidance_tree
+    
 class ChatRoomCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatRoom
@@ -20,8 +22,10 @@ class ChatRoomCreateSerializer(serializers.ModelSerializer):
             'client',
             'guidance_tree',
         ]
-    def validate_guidance_tree(self, guidance_tree):
-        owner = self.initial_data.get('owner')
-        if guidance_tree.owner.pk != owner:
-            raise ValidationError("the owner of the tree and the chatroom should be the same of the guidance tree")
 
+    def validate_guidance_tree(self, guidance_tree):
+        request = self.context.get("request")
+        owner = request.user
+        if guidance_tree.owner != owner:
+            raise ValidationError("the owner of the tree and the chatroom should be the same of the guidance tree")
+        return guidance_tree
