981b03a377c2576cb4d8836543b8f51af1c28c60
HosseinAskarin <hosseinask076@gmail.com>
2023-08-24 03:14:07 +0330

    function for openai response

--- django_backend/scripts/openai.py
+++ django_backend/scripts/openai.py
@@ -7,14 +7,14 @@ environ.Env.read_env()
 
 openai.api_key=env('OPENAI_API_KEY')
 
-prompt=""
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": ""},
-    {"role": "user", "content": prompt}
-  ]
-#   temperature=0,
-#   max_tokens=256,
-)
-print(response.choices[0].message)
+def OpenaiResponse(prompt):
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": ""},
+        {"role": "user", "content": prompt}
+    ]
+    #   temperature=0,
+    #   max_tokens=256,
+    )
+    return response['choices'][0]['text']
