3377f459f9423a1e3233fe56c638c9f7f7756098
barbodak <barbodak@gmail.com>
2023-08-24 00:08:38 +0330

    Responce Node Added

---
+++ sveltekit_frontend/src/lib/components/ResponseNode.svelte
@@ -0,0 +1,56 @@
+
+<script>
+    import { Node, Anchor } from "svelvet";
+    import pkey from '$lib/components/PKEY.svelte';
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+    export let id;
+    export let father;
+    export let position = {x, y};
+    function addChild() {
+        dispatch('add-child');
+    }
+    function selfDistruct() {
+        dispatch('self-distruct');
+    }
+</script>
+
+<Node id={id} position = {position}>
+    <div class="nodeWrapper">
+        <div
+            class="row-auto shadow-orange-400 bg-slate-500 p-1 rounded-sm bg-opacity-40"
+        >
+            <div class="flex flex-row mb-1.5">
+                <textarea
+                    type="text"
+
+                    rows="5"
+                    placeholder="User's Question "
+                />
+                <button class="bg-green-500 w-5 h-auto shadow-md ml-0.5" on:click{addChild}>
+                    +
+                </button>
+            </div>
+            <div class="grid justify-items-stretch">
+                {#if id != 1}
+                    <div class="justify-self-start">
+                        <Anchor edge={pkey} id={id + "i"} locked={true} direction="west" connections={[[father, father + "o"]]}/>
+                    </div>
+                {/if}
+                <div class="justify-self-end">
+                    <Anchor nodeConnect={false} locked={true} direction="east" id={id + "o"} />
+                </div>
+            </div>
+            <div class="flex flex-row mt-1.5">
+                <textarea
+                    type="text"
+                    rows="5"
+                    placeholder="Expected Answer {id}"
+                />
+                <button class="bg-red-500 w-5 h-auto shadow-md ml-0.5" on:click{selfDistruct}>
+                    x
+                </button>
+            </div>
+        </div>
+    </div>
+</Node>
