ae4b9abd830b66108b1b0ad764a28c23f3747adb
barbodak <barbodak@gmail.com>
2023-08-24 01:52:46 +0330

    beutifull buttons

--- sveltekit_frontend/src/lib/components/ResponseNode.svelte
+++ sveltekit_frontend/src/lib/components/ResponseNode.svelte
@@ -16,7 +16,7 @@
     }
 </script>
 
-<Node id={"_" + id } bind:position>
+<Node id={"_" + id} bind:position>
     <div class="nodeWrapper">
         <div
             class="row-auto shadow-orange-400 bg-slate-500 p-1 rounded-sm bg-opacity-40"
@@ -29,7 +29,7 @@
                     placeholder="User's Question "
                 />
                 <button
-                    class="bg-green-500 w-5 h-auto shadow-md ml-0.5"
+                    class="bg-[#56860F] w-5 h-auto ml-0.5 inline-block rounded bg-success text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#14a44d] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2) ] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(20,164,77,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)]"
                     on:click={addChild}
                 >
                     +
@@ -64,7 +64,7 @@
                     placeholder="Expected Answer {id}"
                 />
                 <button
-                    class="bg-red-500 w-5 h-auto shadow-md ml-0.5"
+                    class="bg-red-700 w-5 h-auto ml-0.5 inline-block rounded bg-danger text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#dc4c64] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:bg-danger-600 focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:outline-none focus:ring-0 active:bg-danger-700 active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(220,76,100,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)]"
                     on:click={removeNode}
                 >
                     x
