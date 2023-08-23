639b3ceeb84a4b5452ef4b278958f81a4056abbf
Ahoorast <AhooraBozorgManesh@Gmail.com>
2023-08-24 00:33:27 +0330

    canvas logic fix

--- sveltekit_frontend/src/lib/components/Canvas.svelte
+++ sveltekit_frontend/src/lib/components/Canvas.svelte
@@ -9,24 +9,34 @@
     let parent_array = [-1, 0, 0, 1];
     let example_input_array = ["", "", "", ""];
     let example_output_array = ["", "", "", ""];
+    $: console.log(position_array[0])
     function handleAddChild(id) {
-
+        console.log(id);
     }
     function handleRemoveChild(id) {
-        
+        console.log(id);
     }
 </script>
 
 <svelte:window bind:innerWidth={screenSize} />
 <Svelvet fitView controls minimap height={760}>
     {#each parent_array as par, indx}
+        {#if indx > 0}
         <ResponseNode id={indx} father={par} 
         bind:position={position_array[indx]}
         bind:example_input={example_input_array[indx]}
         bind:example_output={example_output_array[indx]}
         on:add-child={()=> handleAddChild(indx)}
-        on:remove-child={()=> handleRemoveChild(indx)}
+        on:remove-node={()=> handleRemoveChild(indx)}
         />
+        {:else}
+        <ResponseNode id={indx} 
+        bind:position={position_array[indx]}
+        bind:example_input={example_input_array[indx]}
+        bind:example_output={example_output_array[indx]}
+        on:add-child={()=> handleAddChild(indx)}
+        />
+        {/if}
     {/each}
     <ThemeToggle main="light" alt="dark" slot="toggle" />
 </Svelvet>
