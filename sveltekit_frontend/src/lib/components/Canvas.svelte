678583940625c9d096801f9cc2225347ec53f1a4
barbodak <barbodak@gmail.com>
2023-08-24 00:08:38 +0330

    Responce Node Added

--- sveltekit_frontend/src/lib/components/Canvas.svelte
+++ sveltekit_frontend/src/lib/components/Canvas.svelte
@@ -1,18 +1,19 @@
 <script>
-    import { Node, Svelvet } from 'svelvet';
+    import { Node, Svelvet, ThemeToggle, Anchor } from "svelvet";
+        import ResponseNode from "$lib/components/ResponseNode.svelte";
+    let screenSize;
+    let position1 = {x: 0, y: 0};
+    let position2 = {x: 400, y: 400};
+    let position3 = {x: 800, y: 800};
+    let position4 = {x: 0, y: 800};
 </script>
 
-<Svelvet class="w-full h-full" height="{1000}"TD minimap locked>
-    <Node let:grabHandle locked={true} id="alpha" bgColor="red" label="Default Node">
-        <!-- <div class="">
-            <div class="w-full">
-                <textarea class="w-full" type="text" rows="5"                 placeholder="sample expected request leading to this e.g. "/>
-            </div>
-            <div class="w-full">
-                <textarea class="w-full" type="text" rows="5" 
-                placeholder="sample expected response for this e.g. "/>
-            </div>
-        </div> -->
-    </Node>
+<svelte:window bind:innerWidth={screenSize} />
+<p>{position1.x} + {position1.y}</p>
+<Svelvet fitView controls minimap height={760}>
+    <ResponseNode id = {1} position={position1}/>
+    <ResponseNode id = {2} father = {1} position={position2} />
+    <ResponseNode id = {3} father = {1} position={position3} />
+    <ResponseNode id = {4} father = {2} position={position4} />
+    <ThemeToggle main="light" alt="dark" slot="toggle" />
 </Svelvet>
-
---
+++ sveltekit_frontend/src/lib/components/PKEY.svelte
@@ -0,0 +1,5 @@
+<script>
+    import {Edge} from 'svelvet';
+</script>
+
+<Edge let:path animate color=black width='3' ></Edge>
