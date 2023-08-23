2e4656d13afab339a1beac060e2c3656292e301e
Ahoorast <AhooraBozorgManesh@Gmail.com>
2023-08-24 00:21:03 +0330

    better canvas render

--- sveltekit_frontend/src/lib/components/Canvas.svelte
+++ sveltekit_frontend/src/lib/components/Canvas.svelte
@@ -1,19 +1,32 @@
 <script>
     import { Node, Svelvet, ThemeToggle, Anchor } from "svelvet";
-        import ResponseNode from "$lib/components/ResponseNode.svelte";
+    import ResponseNode from "$lib/components/ResponseNode.svelte";
     let screenSize;
-    let position1 = {x: 0, y: 0};
-    let position2 = {x: 400, y: 400};
-    let position3 = {x: 800, y: 800};
-    let position4 = {x: 0, y: 800};
+    let position_array = [{x: 0, y: 0},
+                          {x: 400, y: 400},
+                          {x: 800, y: 800},
+                          {x: 0, y: 800}];
+    let parent_array = [-1, 0, 0, 1];
+    let example_input_array = ["", "", "", ""];
+    let example_output_array = ["", "", "", ""];
+    function handleAddChild(id) {
+
+    }
+    function handleRemoveChild(id) {
+        
+    }
 </script>
 
 <svelte:window bind:innerWidth={screenSize} />
-<p>{position1.x} + {position1.y}</p>
 <Svelvet fitView controls minimap height={760}>
-    <ResponseNode id = {1} position={position1}/>
-    <ResponseNode id = {2} father = {1} position={position2} />
-    <ResponseNode id = {3} father = {1} position={position3} />
-    <ResponseNode id = {4} father = {2} position={position4} />
+    {#each parent_array as par, indx}
+        <ResponseNode id={indx} father={par} 
+        bind:position={position_array[indx]}
+        bind:example_input={example_input_array[indx]}
+        bind:example_output={example_output_array[indx]}
+        on:add-child={()=> handleAddChild(indx)}
+        on:remove-child={()=> handleRemoveChild(indx)}
+        />
+    {/each}
     <ThemeToggle main="light" alt="dark" slot="toggle" />
 </Svelvet>
