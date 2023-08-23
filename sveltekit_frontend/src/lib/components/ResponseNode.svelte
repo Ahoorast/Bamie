852f57e92532071d56fc184b422735e5d1a822f5
barbodak <barbodak@gmail.com>
2023-08-24 00:50:55 +0330

    indent Improvment

--- sveltekit_frontend/src/lib/components/ResponseNode.svelte
+++ sveltekit_frontend/src/lib/components/ResponseNode.svelte
@@ -1,55 +1,72 @@
-
 <script>
     import { Node, Anchor } from "svelvet";
-    import pkey from '$lib/components/PKEY.svelte';
-    import { createEventDispatcher } from 'svelte';
+    import pkey from "$lib/components/PKEY.svelte";
+    import { createEventDispatcher } from "svelte";
     const dispatch = createEventDispatcher();
     export let id;
     export let father;
-    export let position = {x, y};
+    export let position = { x, y };
     export let example_input;
     export let example_output;
     function addChild() {
-        dispatch('add-child');
+        dispatch("add-child");
     }
     function removeNode() {
-        dispatch('remove-node');
+        dispatch("remove-node");
     }
 </script>
 
-<Node id={id} bind:position={position}>
+<Node {id} bind:position>
     <div class="nodeWrapper">
         <div
             class="row-auto shadow-orange-400 bg-slate-500 p-1 rounded-sm bg-opacity-40"
         >
             <div class="flex flex-row mb-1.5">
-                <textarea bind:value={example_input}
+                <textarea
+                    bind:value={example_input}
                     type="text"
-
                     rows="5"
                     placeholder="User's Question "
                 />
-                <button class="bg-green-500 w-5 h-auto shadow-md ml-0.5" on:click={addChild}>
+                <button
+                    class="bg-green-500 w-5 h-auto shadow-md ml-0.5"
+                    on:click={addChild}
+                >
                     +
                 </button>
             </div>
             <div class="grid justify-items-stretch">
-                {#if id != 0}
+                {#if id != 1}
                     <div class="justify-self-start">
-                        <Anchor edge={pkey} id={id + "i"} locked={true} direction="west" connections={[[father, father + "o"]]}/>
+                        <Anchor
+                            edge={pkey}
+                            id={id + "i"}
+                            locked={true}
+                            direction="west"
+                            connections={[[father, father + "o"]]}
+                        />
                     </div>
                 {/if}
                 <div class="justify-self-end">
-                    <Anchor nodeConnect={false} locked={true} direction="east" id={id + "o"} />
+                    <Anchor
+                        nodeConnect={false}
+                        locked={true}
+                        direction="east"
+                        id={id + "o"}
+                    />
                 </div>
             </div>
             <div class="flex flex-row mt-1.5">
-                <textarea bind:value={example_output}
+                <textarea
+                    bind:value={example_output}
                     type="text"
                     rows="5"
                     placeholder="Expected Answer {id}"
                 />
-                <button class="bg-red-500 w-5 h-auto shadow-md ml-0.5" on:click={removeNode}>
+                <button
+                    class="bg-red-500 w-5 h-auto shadow-md ml-0.5"
+                    on:click={removeNode}
+                >
                     x
                 </button>
             </div>
