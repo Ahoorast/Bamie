5b13a9e0a247c10add959e60d89b2214cdf9bf34
Ahoorast <AhooraBozorgManesh@Gmail.com>
2023-08-23 21:30:48 +0330

    better chat page load

--- sveltekit_frontend/src/routes/monitor/[id]/+page.js
+++ sveltekit_frontend/src/routes/monitor/[id]/+page.js
@@ -1,7 +1,4 @@
-import { detail } from "$lib/utils/api/chatroom";
-
 /** @type {import('./$types').PageLoad} */
 export async function load({params}){
-	const data = await detail(params.id);
-    return {...data};
+	return {id: params.id};
 }
