0d75c6de04bdc3850c2b26bc0dc18d6c7015ae11
barbodak <barbodak@gmail.com>
2023-08-25 00:02:48 +0330

    Header added

--- sveltekit_frontend/src/routes/+page.svelte
+++ sveltekit_frontend/src/routes/+page.svelte
@@ -1,7 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import BamieHeader from "$lib/components/header.svelte";
+</script>
 
-<div class="w-1/3">
-    <input class="input variant-form-material input-success"/>
-<input class="input" type="text"/>
-</div>
+<BamieHeader />
