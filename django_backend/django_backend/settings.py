9a9c4084324c04d21ec9a5ae85a4b9879956dd24
HosseinAskarin <hosseinask076@gmail.com>
2023-08-31 16:34:03 +0330

    environmnet and run command

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+97b_tejfs&0n1tb7i+lw=ohr1!8gp+0no@m$#(r__=f%yf8mv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", default=0)
 
 ALLOWED_HOSTS = []
 
@@ -88,15 +88,27 @@ WSGI_APPLICATION = 'django_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DATABASE_NAME'), 
-        'USER': env('DATABASE_USER'), 
-        'PASSWORD': env('DATABASE_PASS'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DATABASE_NAME'), 
+#         'USER': env('DATABASE_USER'), 
+#         'PASSWORD': env('DATABASE_PASS'),
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
